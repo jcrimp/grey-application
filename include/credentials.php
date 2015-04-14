507f4ba4b905b730a909d4f24406e6def4c64c07
Jenny Crimp <jenny.crimp@gmail.com>
2015-04-13 18:50:09 -0700

    Deleted

--- include/credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-//credentials.php
-
-define('DB_NAME','madeby_db');
-define('DB_USER','madeby_admin');
-define('DB_PASSWORD','.3Jade.9Plant');
-define('DB_HOST','mysql.madebyjennycrimp.com');
